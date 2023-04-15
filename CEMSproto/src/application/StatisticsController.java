0aa9ca32230ae2ec0f02befbc9e53e7385fb58d9
Shay Garbuz <shay.garbuz@e.braude.ac.il>
2023-04-15 12:46:14 +0300

    new design - Reports

--- CEMSproto/src/application/StatisticsController.java
+++ CEMSproto/src/application/StatisticsController.java
@@ -29,4 +29,26 @@ public class StatisticsController {
             ex.printStackTrace();
         }
 	}
+	public void LogOut(ActionEvent e) {
+		try {
+			
+            Parent CMEMS_Root = FXMLLoader.load(getClass().getResource("CEMS.fxml"));
+            Scene CMEMS_Scene = new Scene(CMEMS_Root);
+            Stage  CMEMS_Stage = new Stage();
+            CMEMS_Stage.setTitle("Login Window");
+            CMEMS_Stage.setScene(CMEMS_Scene);
+            CMEMS_Stage.show();
+
+            Node source = (Node) e.getSource();
+            
+            // Get the Stage object that contains the source node
+            Stage stage = (Stage) source.getScene().getWindow();
+            
+            // Close the stage
+            stage.close();
+           
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
 }
