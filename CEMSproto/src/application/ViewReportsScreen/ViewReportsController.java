79b52fd3c3eafd9b0a700e34e70f87f09254ef0a
Fredi Bulshtein <Fredi1574@gmail.com>
2023-05-08 19:58:10 +0300

    cosmetic changes

--- CEMSproto/src/application/ViewReportsScreen/ViewReportsController.java
+++ CEMSproto/src/application/ViewReportsScreen/ViewReportsController.java
@@ -21,22 +21,12 @@ public class ViewReportsController {
     @FXML
     private ComboBox<Integer> yearComboBox;
     @FXML
-    private ComboBox<Character> semesterComboBox;
+    private ComboBox<String> semesterComboBox;
     @FXML
     private ComboBox<String> subjectComboBox;
 
     @FXML
     private TableView<Report> reportsTableView;
-    @FXML
-    private TableColumn<Report, String> yearColumn;
-    @FXML
-    private TableColumn<Report, String> semesterColumn;
-    @FXML
-    private TableColumn<Report, String> subjectColumn;
-    @FXML
-    private TableColumn<Report, String> courseColumn;
-    @FXML
-    private TableColumn<Report, String> testIdColumn;
 
     @FXML
     public void initialize() {
@@ -45,27 +35,30 @@ public class ViewReportsController {
         ObservableList<Integer> yearList = FXCollections.observableArrayList(2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015);
         ObservableList<Character> semesterList = FXCollections.observableArrayList('A', 'B', 'S');
         ObservableList<String> subjectList = FXCollections.observableArrayList("Computer Science", "Mathematics",
-                "Algorithms", "Statistics", "Bananas and monkeys", "Introduction to Databases");
+                "Algorithms", "Statistics", "Bananas n' monkeys", "Introduction to Databases");
 
         yearComboBox.setItems(yearList);
         semesterComboBox.setItems(semesterList);
         subjectComboBox.setItems(subjectList);
 
         ObservableList<Report> reportList = FXCollections.observableArrayList(
-                new Report(2023, 'A', "Computer Science", "Automatons", 42069),
-                new Report(2021, 'B', "Mathematics", "Statistics", 13096)
+                new Report(2023, "A", "Computer Science", "Automatons", 42069),
+                new Report(2021, "B", "Mathematics", "Statistics", 13096),
+                new Report(2021, "Summer", "Statistics", "Bananas n' monkeys", 13099)
         );
 
         ObservableList<String> columnList = FXCollections.observableArrayList();
-        columnList.addAll("Year", "Semester", "Subject", "Course", "TestId");
+        columnList.addAll("Year", "Semester", "Subject", "Course", "Test ID");
 
         ObservableList<TableColumn<Report, ?>> columns = TableManager.createTable(reportsTableView, columnList, reportList);
         TableManager.addDoubleClickFunctionality(reportsTableView, "ViewReportsScreen/GraphScreen/ViewGraph.fxml");
 
         //todo: find a cleaner way to do change the width of every column
         for (TableColumn<Report, ?> column : columns) {
-            if (column.getText().equals("Subject")) {
-                column.prefWidthProperty().bind(reportsTableView.widthProperty().multiply(0.594));
+            if (column.getText().equals("Course")) {
+                column.prefWidthProperty().bind(reportsTableView.widthProperty().multiply(0.544));
+            } else if (column.getText().equals("Subject")) {
+                column.prefWidthProperty().bind(reportsTableView.widthProperty().multiply(0.15));
             } else {
                 column.prefWidthProperty().bind(reportsTableView.widthProperty().multiply(0.1));
             }
