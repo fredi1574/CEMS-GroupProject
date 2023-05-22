d3b8341e44279774228350818868e5fee2ad5e18
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-05-22 17:55:18 +0300

    add class loggedin

---
+++ CEMS/src/entity/LoggedInUser.java
@@ -0,0 +1,26 @@
+package entity;
+
+public class LoggedInUser extends User {
+    private static User authenticatedUser;
+
+    public LoggedInUser(String id, String firstName, String lastName, String userName, String password, String email) {
+        super(id, firstName, lastName, userName, password, email);
+    }
+
+    public static User getAuthenticatedUser() {
+        return authenticatedUser;
+    }
+
+    public static void setAuthenticatedUser(User user) {
+        authenticatedUser = user;
+    }
+
+    public String getName() {
+        return getFirstName();
+    }
+
+    @Override
+    public Role getRole() {
+        return super.getRole();
+    }
+}
