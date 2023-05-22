96635e652be837ea4abb211eeb1c9a72d8fae47d
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-05-22 20:06:51 +0300

    remove unused import + comments + updatelogin

--- CEMS/src/application/loginWindowScreen/LoginWindowController.java
+++ CEMS/src/application/loginWindowScreen/LoginWindowController.java
@@ -14,10 +14,10 @@ import util.ScreenManager;
 
 import java.sql.*;
 
-import static javafx.scene.control.Alert.AlertType.CONFIRMATION;
 
 public class LoginWindowController {
 
+    // Database connection details
     private static final String DB_URL = "jdbc:mysql://localhost:3306/cems?serverTimezone=UTC&useSSL=false";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "Aa123456";
@@ -29,25 +29,31 @@ public class LoginWindowController {
     @FXML
     private PasswordField passwordField;
 
-
+    // This method is called when the FXML file is loaded
     public void initialize() {
+        // Enables dragging and dropping of the application window using the header pane
         ScreenManager.dragAndDrop(header);
     }
+
+    // Event handler for login button click
     @FXML
     public void logIN(ActionEvent event) {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
+        // Check if username or password fields are empty
         if (username.isEmpty() || password.isEmpty()) {
             showAlertDialog(AlertType.WARNING, "Incomplete Fields", "Please enter both username and password.");
             return;
         }
 
+        // Authenticate user and retrieve their role
         String role = authenticateUser(username, password);
         if (role != null) {
+            // Redirect to the appropriate screen based on the user's role
             switch (role) {
                 case "Student":
-                    ScreenManager.goToNewScreen(event,PathConstants.manageQuestions);
+                    ScreenManager.goToNewScreen(event, PathConstants.manageQuestions);
                     break;
                 case "Lecturer":
                     ScreenManager.goToNewScreen(event, PathConstants.mainMenuPath);
@@ -64,14 +70,17 @@ public class LoginWindowController {
         }
     }
 
+    // Event handler for text click
     public void handleTextClick() {
+        // Show an information dialog with a message
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Information");
         alert.setHeaderText(null);
-        alert.setContentText("to reset password please contact our admin");
+        alert.setContentText("To reset password, please contact our admin.");
         alert.showAndWait();
     }
 
+    // Authenticate user by checking their credentials in the database
     private String authenticateUser(String username, String password) {
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              PreparedStatement statement = connection.prepareStatement("SELECT role FROM users WHERE username = ? AND password = ?")) {
@@ -88,7 +97,7 @@ public class LoginWindowController {
         return null;
     }
 
-
+    // Show an alert dialog with the specified alert type, title, and message
     private void showAlertDialog(AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
@@ -97,13 +106,17 @@ public class LoginWindowController {
         alert.showAndWait();
     }
 
+    // Event handler for close button click
     @FXML
     private void closeClient(ActionEvent event) {
+        // Close the application window
         ExitButton.closeClient(event);
     }
 
+    // Event handler for minimize button click
     @FXML
     public void minimizeWindow(ActionEvent event) {
+        // Minimize the application window
         MinimizeButton.minimizeWindow(event);
     }
 }
