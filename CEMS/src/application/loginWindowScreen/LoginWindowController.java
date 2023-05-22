d0742782b222ea25a75620a46ae60b0996a0547b
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-05-22 18:04:01 +0300

    Navigation by user role

--- CEMS/src/application/loginWindowScreen/LoginWindowController.java
+++ CEMS/src/application/loginWindowScreen/LoginWindowController.java
@@ -33,22 +33,37 @@ public class LoginWindowController {
     public void initialize() {
         ScreenManager.dragAndDrop(header);
     }
-
     @FXML
     public void logIN(ActionEvent event) {
-//        String username = usernameField.getText();
-//        String password = passwordField.getText();
-//
-//        if (username.isEmpty() || password.isEmpty()) {
-//            showAlertDialog(AlertType.WARNING, "Incomplete Fields", "Please enter both username and password.");
-//            return;
-//        }
-//        if (authenticateUser(username, password)) {
-         ScreenManager.goToNewScreen(event, PathConstants.mainMenuPath);
-//        } else {
-//            showAlertDialog(AlertType.ERROR, "Authentication Failed", "Invalid username or password.");
-//        }
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            showAlertDialog(AlertType.WARNING, "Incomplete Fields", "Please enter both username and password.");
+            return;
+        }
+
+        String role = authenticateUser(username, password);
+        if (role != null) {
+            switch (role) {
+                case "Student":
+                    ScreenManager.goToNewScreen(event,PathConstants.manageQuestions);
+                    break;
+                case "Lecturer":
+                    ScreenManager.goToNewScreen(event, PathConstants.mainMenuPath);
+                    break;
+                case "HeadOfDepartment":
+                    ScreenManager.goToNewScreen(event, PathConstants.viewReportsPath);
+                    break;
+                default:
+                    showAlertDialog(AlertType.ERROR, "Authentication Failed", "Invalid username or password.");
+                    break;
+            }
+        } else {
+            showAlertDialog(AlertType.ERROR, "Authentication Failed", "Invalid username or password.");
+        }
     }
+
     public void handleTextClick() {
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Information");
@@ -57,22 +72,22 @@ public class LoginWindowController {
         alert.showAndWait();
     }
 
-    private boolean authenticateUser(String username, String password) {
+    private String authenticateUser(String username, String password) {
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement statement = connection.prepareStatement("SELECT COUNT(*) FROM users WHERE username = ? AND password = ?")) {
+             PreparedStatement statement = connection.prepareStatement("SELECT role FROM users WHERE username = ? AND password = ?")) {
             statement.setString(1, username);
             statement.setString(2, password);
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
-                    int count = resultSet.getInt(1);
-                    return count > 0;
+                    return resultSet.getString("role");
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
+
 
     private void showAlertDialog(AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
