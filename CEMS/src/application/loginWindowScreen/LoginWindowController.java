96d5c37c5404bb05af479e0e9b13c503d866acea
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2023-05-25 18:19:49 +0300

    MsgHandler Login = new MsgHandler current position

--- CEMS/src/application/loginWindowScreen/LoginWindowController.java
+++ CEMS/src/application/loginWindowScreen/LoginWindowController.java
@@ -6,8 +6,6 @@ import common.TypeMsg;
 import entity.LoggedInUser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
