197d54ccf1db0a66daa1342166ead8ddc930abc7
May Caspi <maycas@gmail.com>
2023-06-09 18:57:23 +0300

    added Popup screen functioanlity

--- CEMS/src/application/Simulation/SmsEmailPopUpController.java
+++ CEMS/src/application/Simulation/SmsEmailPopUpController.java
@@ -1,11 +1,13 @@
 package application.Simulation;
 
+import client.Client;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import util.MinimizeButton;
+import util.ScreenManager;
 
 public class SmsEmailPopUpController {
 
@@ -23,8 +25,14 @@ public class SmsEmailPopUpController {
 
     @FXML
     void closeClient(ActionEvent event) {
-
+        ScreenManager.dragAndDrop(header);
+        fullNameText.setText(Client.user.getFullName());
+        EmailAdressTxt.setText(Client.user.getEmail());
     }
+    public void SetInfoField(String info){
+        InfoTextField.setText(info);
+    }
+
 
     @FXML
     public void minimizeWindow(ActionEvent event) {
